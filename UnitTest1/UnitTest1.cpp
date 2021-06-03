5c7960cba73b7f974fbd587720d982d3dd159642
AlexKovtunn <can9i2000@bk.ru>
2021-06-03 21:25:05 +0300

    1

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../kyrsovaya/EdKarp.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(first)
+		{
+			EdKarp* t = new EdKarp();
+			t->readList("in.txt");
+			cout << t->MaxFlow();
+		}
+	};
+}
